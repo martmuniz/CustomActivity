ef806d63968a33ec81be1025c22198313f33ce36
Martin Muniz <49213975+martmuniz@users.noreply.github.com>
2025-02-12 11:09:11 +0100

    update

--- activity.js
+++ activity.js
@@ -4,12 +4,12 @@ var connection = new Postmonger.Session();
 connection.trigger('ready');
 
 connection.on('initActivity', function(data) { 
-    document.getElementById('message').value = JSON.stringify( data, null, 2);
+    document.getElementById('configuration').value = JSON.stringify( data, null, 2);
 });
 
 // Save Sequence
 connection.on('clickedNext', function() {
-    var configuration = JSON.parse(document.getElementById('message').value);
+    var configuration = JSON.parse(document.getElementById('configuration').value);
     connection.trigger('updateActivity', configuration);
 });
 
