7e82cf02d12bf6d57c811cd23250566101e864f6
Martin Muniz <49213975+martmuniz@users.noreply.github.com>
2025-02-13 14:02:25 +0100

    a

--- activity.js
+++ activity.js
@@ -1,22 +1,28 @@
 var connection = new Postmonger.Session();
 var payload = {};
 
-//Startup Sequence 
+// Startup Sequence 
 connection.trigger('ready');
 
 connection.on('initActivity', function(data) { 
     if (data) {
         payload = data;
     }
-    document.getElementById('key').value = payload["arguments"].execute.inArguments[0].customerKey;
+    // Check if customerKey exists before assigning
+    if (payload["arguments"] && payload["arguments"].execute && payload["arguments"].execute.inArguments[0].customerKey) {
+        document.getElementById('key').value = payload["arguments"].execute.inArguments[0].customerKey;
+    }
 });
 
 // Save Sequence
 connection.on('clickedNext', function() {
     var key = document.getElementById('key').value.trim();
 
-    payload["arguments"].execute.inArguments[0].customerKey = key;
-
-    connection.trigger('updateActivity', payload);
+    // Ensure key is not empty
+    if (key) {
+        payload["arguments"].execute.inArguments[0].customerKey = key;
+        connection.trigger('updateActivity', payload);
+    } else {
+        alert('Customer Key cannot be empty');
+    }
 });
-
