b48d06bdae0bb14c64c62d8a566fb61a1150f60a
Martin Muniz <49213975+martmuniz@users.noreply.github.com>
2025-03-02 14:33:46 +0100

    cambios

--- activity.js
+++ activity.js
@@ -3,16 +3,31 @@ var payload = {};
 
 // Startup Sequence 
 connection.trigger('ready');
-connection.trigger('requestSchema');
-connection.on('requestedSchema', function (data) {
-    // save schema
-    console.log('*** Schema ***', JSON.stringify(data['schema']));
- });
 
 connection.on('initActivity', function(data) { 
     if (data) {
         payload = data;
     }
+    connection.trigger('requestSchema');
+    connection.on('requestedSchema', function (data) {
+        
+    // save schema
+    console.log('*** Schema ***', JSON.stringify(data['schema']));
+
+    // add entry source attributes as inArgs
+    const schema = data['schema'];
+
+    for (var i = 0, l = schema.length; i < l; i++) {
+        var inArg = {};
+        let attr = schema[i].key;
+        let keyIndex = attr.lastIndexOf('.') + 1;
+        inArg[attr.substring(keyIndex)] = '{{' + attr + '}}';
+        payload['arguments'].execute.inArguments.push(inArg);
+    }
+  });
+
+  let argArr = payload['arguments'].execute.inArguments;
+  
     // Check if customerKey exists before assigning
     if (payload["arguments"] && payload["arguments"].execute && payload["arguments"].execute.inArguments[0].message) {
         document.getElementById('key').value = payload["arguments"].execute.inArguments[0].message;
