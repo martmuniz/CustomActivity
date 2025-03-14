334fdc64edf0d238fd294d2fa5c47af094833e22
Martin Muniz <49213975+martmuniz@users.noreply.github.com>
2025-03-14 12:37:56 +0100

    Log1

--- activity.js
+++ activity.js
@@ -1,5 +1,18 @@
 var connection = new Postmonger.Session();
 var payload = {};
+const fs = require('fs');
+const path = require('path');
+const logFilePath = path.join(__dirname, 'log.txt');
+
+// Función para registrar en el archivo log.txt
+function logApiCall(data) {
+    const logEntry = `${new Date().toISOString()} - ${JSON.stringify(data)}\n`;
+    fs.appendFile(logFilePath, logEntry, (err) => {
+        if (err) {
+            console.error('Error al escribir en el archivo log.txt:', err);
+        }
+    });
+}
 
 // Startup Sequence 
 connection.trigger('ready');
@@ -36,13 +49,16 @@ connection.on('clickedNext', function () {
         payload["arguments"].execute = {
             inArguments: newInArguments,
             url: payload["arguments"].execute.url, // Mantiene la URL de ejecución
-            verb: "POST" ,
+            verb: "POST",
             "timeout": 1000
         };
 
         payload["metaData"].isConfigured = true;
 
         console.log("Payload enviado:", JSON.stringify(payload));
+
+        // Registrar la llamada API en el archivo log.txt
+        logApiCall(payload);
 
         connection.trigger('updateActivity', payload);
     } else {
